8fd8791b61afc54f8a6e9080014363348c331473
rosacf-ga <rc.floress@yahoo.com>
2022-04-08 14:40:29 -0700

    Mount routes to server with appropriate base paths

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const logger = require('morgan');
@@ -6,7 +7,11 @@ const cookieParser = require('cookie-parser');
 const session = require('express-session');
 const passport = require('passport');
 const methodOverride = require('method-override');
-const indexRoutes = require('./routes/index');
+//require routes files
+const indexRouter = require('./routes/index');
+const programsRouter = require('./routes/programs');
+const reviewsRouter = require('./routes/reviews');
+
 // load the env consts
 require('dotenv').config();
 
@@ -43,18 +48,30 @@ app.use(passport.session());
 
 // Add this middleware BELOW passport middleware
 app.use(function (req, res, next) {
-  res.locals.user = req.user; // assinging a property to res.locals, makes that said property (user) availiable in every
-  // single ejs view
+  res.locals.user = req.user; // assigning a property to res.locals, makes that said property (user) availiable in every single ejs view
   next();
 });
 
 // mount all routes with appropriate base paths
-app.use('/', indexRoutes);
+app.use('/programs', programsRouter); 
+app.use('/', reviewsRouter); //nested documents always mounted to '/'
+app.use('/', indexRouter);
 
 
-// invalid request, send 404 page
-app.use(function(req, res) {
-  res.status(404).send('Cant find that!');
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;
