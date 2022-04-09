1bf6a2299fa6c88ccac5ccc22a1d02e5502b4ba3
rosacf-ga <rc.floress@yahoo.com>
2022-04-09 15:29:55 -0700

    Create show route

--- routes/programs.js
+++ routes/programs.js
@@ -6,5 +6,6 @@ const programsCtrl = require('../controllers/programs');
 router.get('/', programsCtrl.index);
 router.get('/new', programsCtrl.new);
 router.post('/', programsCtrl.create);
+router.get('/:id',programsCtrl.show);
 
 module.exports = router;
