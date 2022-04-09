a8d7aa19ed814eb673e819abaffa42663984047f
rosacf-ga <rc.floress@yahoo.com>
2022-04-09 13:11:24 -0700

    Add min and max to review rating

--- models/program.js
+++ models/program.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
   content: {type: String, required: true},
-  rating: {type: Number, required: true},
+  rating: {type: Number, min: 1, max: 5, required: true},
   user: {type: Schema.Types.ObjectId, ref: 'User'},
   userName: String, 
   userAvatar: String
