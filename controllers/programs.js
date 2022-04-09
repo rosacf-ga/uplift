711a85e57ea3ea89b4e237accd7136db326b223b
rosacf-ga <rc.floress@yahoo.com>
2022-04-09 15:28:47 -0700

    Create show function to render a program's show page

--- controllers/programs.js
+++ controllers/programs.js
@@ -3,10 +3,13 @@ const Program = require ('../models/program')
 module.exports = {
   index, 
   new: newProgram,
-  create
+  create,
+  show
 }
 
 function index(req, res){
+  console.log(req.user, '<-req.user')
+  //finds/will display all programs
   Program.find({}, function(err, programs){ 
     res.render('programs/index', {programs, title: 'All Programs'});
   })
@@ -18,9 +21,17 @@ function newProgram(req,res){
 
 function create(req, res){
   const program = new Program(req.body);
+  program.user = req.user._id;
   console.log(program);
   program.save(function(err){
     if(err) return res.redirect('/programs/new');
-    res.redirect('/programs');
+    res.redirect(`programs/${program._id}`);
+  })
+}
+
+function show(req, res){
+  //req.params.id will be able to target id of specific program we want to view
+  Program.findById(req.params.id, function(err, program){
+    res.render('programs/show', {title: 'Program Details', program})
   })
 }
